89e1e9a73d999af1d71ae98de7d8b2108518c8d9
kush kumar <kushkumarking098765@gmail.com>
2026-03-15 13:20:04 +0530

    fix: refactor db.ts to streamline adapter setup and improve connection handling; update prisma.config.ts to utilize env for datasource URL

--- lib/db.ts
+++ lib/db.ts
@@ -1,29 +1,15 @@
-import { PrismaClient } from "./generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
-
+import { PrismaClient } from "./generated/prisma/client"; 
+import { PrismaPg } from "@prisma/adapter-pg"; 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
-
-// 1. Create a connection pool using the Transaction URL (Port 6543)
-// The connection_limit=1 is vital for serverless to prevent 'too many connections'
-const pool = new Pool({ 
-  connectionString: process.env.DATABASE_URL 
-});
-
-// 2. Initialize the adapter with that pool
-const adapter = new PrismaPg(pool);
-
-// 3. Create the client instance
+  prisma: PrismaClient; 
+}; 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL, 
+}); 
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
-    // Optional: Log queries in development to see what's happening
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+    adapter, 
+  }); 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+export default prisma; 
